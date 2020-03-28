a073b6b4199eef6a4b13e9529f4313bd267bd257
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2020-03-28 16:38:18 -0300

    Implementação da paginação - Partes 1, 2, 3, 4, 5 e 6.

--- src/main/java/br/com/tadeudeveloper/springrestapi/controller/IndexController.java
+++ src/main/java/br/com/tadeudeveloper/springrestapi/controller/IndexController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,121 +30,162 @@ import br.com.tadeudeveloper.springrestapi.service.ImplementacaoUserDetailsServi
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
-	
+
 	@Autowired
-	private UsuarioRepository usuarioRepository;		
-	
+	private UsuarioRepository usuarioRepository;
+
 	@Autowired
 	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
-	
+
 	@Autowired
 	private TelefoneRepository telefoneRepository;
-	
+
 	@GetMapping(value = "/{id}", produces = "application/json")
 	@CachePut("cacheuser")
-	public ResponseEntity<Usuario> buscarPorId(@PathVariable(value = "id") Long id) {		
-		Usuario usuario = usuarioRepository.findById(id).get();			
+	public ResponseEntity<Usuario> buscarPorId(@PathVariable(value = "id") Long id) {
+		Usuario usuario = usuarioRepository.findById(id).get();
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
-	
+
 	@GetMapping(value = "v1/{id}", produces = "application/json")
 	@Cacheable("cacheuser")
-	public ResponseEntity<Usuario> buscarPorIdV1(@PathVariable(value = "id") Long id) {		
-		Usuario usuario = usuarioRepository.findById(id).get();	
+	public ResponseEntity<Usuario> buscarPorIdV1(@PathVariable(value = "id") Long id) {
+		Usuario usuario = usuarioRepository.findById(id).get();
 		System.out.println("Buscar por id V1");
 		return ResponseEntity.ok(usuario);
 	}
-	
+
 	@GetMapping(value = "v2/{id}", produces = "application/json")
-	public ResponseEntity<Usuario> buscarPorIdV2(@PathVariable(value = "id") Long id) {		
-		Usuario usuario = usuarioRepository.findById(id).get();	
+	public ResponseEntity<Usuario> buscarPorIdV2(@PathVariable(value = "id") Long id) {
+		Usuario usuario = usuarioRepository.findById(id).get();
 		System.out.println("Buscar por id V2");
 		return ResponseEntity.ok(usuario);
-	}	
-	
+	}
+
 	@GetMapping(value = "/v1", produces = "application/json", headers = "X-API-Version=v1")
 	@Cacheable("cacheusuarios")
-	public ResponseEntity<List<Usuario>> buscarTodosV1() throws InterruptedException {		
-		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();		
+	public ResponseEntity<List<Usuario>> buscarTodosV1() throws InterruptedException {
+		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
 		System.out.println("Buscar Todos V1");
 		Thread.sleep(6000);
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
-	
+
 	@GetMapping(value = "/v2", produces = "application/json", headers = "X-API-Version=v2")
-	public ResponseEntity<List<Usuario>> buscarTodosV2() {		
-		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();		
+	public ResponseEntity<List<Usuario>> buscarTodosV2() {
+		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
 		System.out.println("Buscar Todos V2");
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
-	
+
 	@GetMapping(value = "/", produces = "application/json")
 	@CachePut("cacheusuarios")
-	public ResponseEntity<List<Usuario>> buscarTodos() {		
-		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();				
-		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	public ResponseEntity<Page<Usuario>> buscarTodos() {
+		PageRequest page = PageRequest.of(0, 5, Sort.by("nome"));
+		Page<Usuario> list = usuarioRepository.findAll(page);
+		return new ResponseEntity<Page<Usuario>>(list, HttpStatus.OK);
 	}
 	
+	@GetMapping(value = "/page/{pagina}", produces = "application/json")
+	@CachePut("cacheusuarios")
+	public ResponseEntity<Page<Usuario>> buscarTodosPaginado(@PathVariable("pagina") int pagina) {
+		PageRequest page = PageRequest.of(pagina, 5, Sort.by("nome"));
+		Page<Usuario> list = usuarioRepository.findAll(page);
+		return new ResponseEntity<Page<Usuario>>(list, HttpStatus.OK);
+	}
+
 	@GetMapping(value = "/usuarioPorNome/{nome}", produces = "application/json")
 	@CachePut("cacheusuarios")
-	public ResponseEntity<List<Usuario>> buscarUsuarioPorNome(@PathVariable("nome") String nome) {		
-		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findByNomeContainingIgnoreCase(nome);				
-		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	public ResponseEntity<Page<Usuario>> buscarUsuarioPorNome(@PathVariable("nome") String nome) {		
+		
+		PageRequest pageRequest = null;
+		Page<Usuario> list = null;
+		
+		if (nome == null || (nome != null && nome.trim().isEmpty())
+				|| nome.equalsIgnoreCase("undefined")) { // não informou o nome
+			pageRequest = PageRequest.of(0, 5, Sort.by("nome"));
+			list = usuarioRepository.findAll(pageRequest);		
+		} else {
+			pageRequest = PageRequest.of(0, 5, Sort.by("nome"));
+			list = usuarioRepository.findUserByNamePage(nome, pageRequest);
+		}		
+		
+		return new ResponseEntity<Page<Usuario>>(list, HttpStatus.OK);
 	}
 	
+	@GetMapping(value = "/usuarioPorNome/{nome}/page/{page}", produces = "application/json")
+	@CachePut("cacheusuarios")
+	public ResponseEntity<Page<Usuario>> buscarUsuarioPorNomePage(
+			@PathVariable("nome") String nome, @PathVariable("page") int page) {		
+		
+		PageRequest pageRequest = null;
+		Page<Usuario> list = null;
+		
+		if (nome == null || (nome != null && nome.trim().isEmpty())
+				|| nome.equalsIgnoreCase("undefined")) { // não informou o nome
+			pageRequest = PageRequest.of(page, 5, Sort.by("nome"));
+			list = usuarioRepository.findAll(pageRequest);		
+		} else {
+			pageRequest = PageRequest.of(page, 5, Sort.by("nome"));
+			list = usuarioRepository.findUserByNamePage(nome, pageRequest);
+		}		
+		
+		return new ResponseEntity<Page<Usuario>>(list, HttpStatus.OK);
+	}
+
 	@PostMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
-		
+
 		// Caso o telefone não seja salvo automaticamente
-		for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
 			usuario.getTelefones().get(pos).setUsuario(usuario);
 		}
-		
+
 		String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
 		usuario.setSenha(senhaCriptografada);
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
-		
+
 		implementacaoUserDetailsService.inserirAcessoPadrao(usuario.getId());
-		
-		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);		
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
-	
+
 	@PutMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {		
-		
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+
 		// Caso o telefone não seja salvo automaticamente
-		for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
 			usuario.getTelefones().get(pos).setUsuario(usuario);
 		}
-		
+
 		Usuario userTemp = usuarioRepository.findById(usuario.getId()).get();
-		
+
 		if (!userTemp.getSenha().equals(usuario.getSenha())) {
 			String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
 			usuario.setSenha(senhaCriptografada);
 		}
-		
-		Usuario usuarioSalvo = usuarioRepository.save(usuario);		
-		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);		
+
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
-	
+
 	@PutMapping(value = "/{id}", produces = "application/json")
-	public ResponseEntity<Usuario> atualizarSetId(@PathVariable("id") Long id, @RequestBody Usuario usuario) {		
+	public ResponseEntity<Usuario> atualizarSetId(@PathVariable("id") Long id, @RequestBody Usuario usuario) {
 		usuario.setId(id);
-		Usuario usuarioSalvo = usuarioRepository.save(usuario);		
-		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping(value = "/{id}", produces = "application/text")
-	public String delete(@PathVariable("id") Long id) {		
-		usuarioRepository.deleteById(id);		
-		return  "ok";		
+	public String delete(@PathVariable("id") Long id) {
+		usuarioRepository.deleteById(id);
+		return "ok";
 	}
-	
+
 	@DeleteMapping(value = "/removerTelefone/{id}", produces = "application/text")
 	public String deleteTelefone(@PathVariable("id") Long id) {
 		telefoneRepository.deleteById(id);
 		return "ok";
 	}
-	
+
 }
--- src/main/java/br/com/tadeudeveloper/springrestapi/repository/UsuarioRepository.java
+++ src/main/java/br/com/tadeudeveloper/springrestapi/repository/UsuarioRepository.java
@@ -2,16 +2,20 @@ package br.com.tadeudeveloper.springrestapi.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.tadeudeveloper.springrestapi.model.Usuario;
 
 @Repository
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	@Query("SELECT u FROM Usuario u WHERE u.login = ?1")
 	Usuario findUserByLogin(String login);	
@@ -25,5 +29,18 @@ public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 	@Modifying
 	@Query(value = "insert into usuarios_role (usuario_id, role_id) values (?1, (select id from role where nome_role = 'ROLE_USER'));", nativeQuery = true)
 	void inserirAcessoRolePadrao(Long idUsuario);
+
+	default Page<Usuario> findUserByNamePage(String nome, PageRequest pageRequest) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+		
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny()
+				.withMatcher("nome", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+		
+		Example<Usuario> example = Example.of(usuario, exampleMatcher);
+		
+		return findAll(example, pageRequest);
+	}
 	
 }
